9c69955b2d702cdf19345e3d9471c58a58c5cca2
Thierno Mamadou DIALLO <thiernomdou63@gmail.com>
2022-04-01 14:49:44 +0200

    appel-api-useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [dataImg, setDataImg] = useState();
+
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setDataImg(data[0].url)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dataImg &&
+        <img src={dataImg}
+          alt="cat image"
+          style={{ width: "500px" }}
+        />
+      }
     </div>
   );
 }
